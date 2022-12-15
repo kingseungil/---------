21f5beeeb5efc08f96c2273c46e46dee7b095cb1
kingseungil <flykim5115@gmail.com>
2022-12-15 16:46:28 +0900

    수정,삭제 구현(비밀번호 비교 적용 X)

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Posts = require("../schemas/post");
+const bcrypt = require("bcrypt");
 
 /**
  * 전체 게시글 목록 조회 API
@@ -21,6 +22,8 @@ router.post("/posts", async (req, res) => {
         author: author,
     });
     await post.save();
+    // console.log(password);
+    // console.log(post.password); // 암호화된 password
     res.status(200).json(post);
 });
 
@@ -34,7 +37,9 @@ router.post("/posts", async (req, res) => {
 // 게시글 조회 API (id값 이용)
 router.get("/posts/:id", async (req, res) => {
     const { id } = req.params;
+
     const result = await Posts.find({ id: id });
+
     res.json(result);
 });
 
@@ -45,27 +50,39 @@ router.get("/posts/:id", async (req, res) => {
 router.put("/posts/:id", async (req, res) => {
     const { id } = req.params;
     const { password, title, content, author } = req.body;
-    const post = Posts.find({ id: id });
-
-    if (post.password !== password) {
-        return res.json({ message: "비밀번호 불일치" });
-    } else {
-        await Posts.findOneAndUpdate(
-            { id: id },
-            {
-                $set: {
-                    title: title,
-                    content: content,
-                    author: author,
-                },
-            }
-        );
-        res.status(200).json({ success: true });
-    }
+    // const post = await Posts.find({ id: id });
+    // const passwordMatch = await bcrypt.compare(password, post.password);
+    // if (!passwordMatch) {
+    //     return res.status(400).json({ message: "비밀번호 불일치" });
+    // }
+    // await Posts.findOneAndUpdate(id, {
+    //     $set: {
+    //         title: title,
+    //         content: content,
+    //         author: author,
+    //     },
+    // });
+    await Posts.findOneAndUpdate(id, {
+        $set: {
+            title: title,
+            content: content,
+            author: author,
+        },
+    });
+    res.json({ success: true });
 });
 
 /**
  * 게시글 삭제 API
  * - 입력된 비밀번호를 확인하여 삭제
  */
+router.delete("/posts/:id", async (req, res) => {
+    const { id } = req.params;
+    const post = await Posts.find({ id });
+    if (post) {
+        await Posts.deleteOne({ id });
+    }
+    res.json({ success: true });
+});
+
 module.exports = router;
